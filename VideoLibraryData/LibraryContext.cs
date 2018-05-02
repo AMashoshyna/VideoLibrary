5bc79f0340914b776e48bd486c00790f6348a0fd
amashoshyna <a.mashoshyna@gmail.com>
2018-05-02 18:48:56 +0300

    add VideoLivraryData project for DB context

---
+++ VideoLibraryData/LibraryContext.cs
@@ -0,0 +1,12 @@
+﻿using VideoLibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace VideoLibraryData
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
