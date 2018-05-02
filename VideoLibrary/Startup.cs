d4fa2c28ffad6e7a83d709b141d3c72f2971e8e1
amashoshyna <a.mashoshyna@gmail.com>
2018-05-02 18:50:22 +0300

    add library context on startUp

--- VideoLibrary/Startup.cs
+++ VideoLibrary/Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using VideoLibraryData;
 
 namespace VideoLibrary
 {
@@ -22,6 +24,7 @@ namespace VideoLibrary
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<LibraryContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
